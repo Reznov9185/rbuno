582e46f5c74947673989c2820336369d68f80d81
Reznov9185 <reznov9185@gmail.com>
2020-04-30 23:16:04 +0600

    Using command line graphics gem - added cli-ui gem - todo: need tweaks

--- lib/rbuno.rb
+++ lib/rbuno.rb
@@ -1,14 +1,25 @@
+require 'cli/ui'
+
 class Rbuno
   def self.play
-    puts "Please draw your card value: "
-    number = gets.chomp.to_i
-    puts "Please draw your card color: "
-    color = gets.chomp.to_s
-    puts "Confirm your card: #{color.capitalize} - #{number}, (y/n)"
-    confirm = gets.chomp.to_s.downcase
-    if confirm == "y"
-    else
-      self.play
+    CLI::UI::StdoutRouter.enable
+    CLI::UI::Frame.open("Ruby's UNO") do
+      print "Please draw your card value: "
+      number = gets.chomp.to_i
+      print "Please draw your card color: "
+      color = gets.chomp.to_s
+      confirm = CLI::UI::Prompt.ask(
+        "Confirm your card: #{color.capitalize} - #{number}"
+      ) do |handler|
+        handler.option('Yes')  { |selection| selection }
+        handler.option('No')  { |selection| selection }
+      end
+      confirm = confirm.downcase
+      if confirm == "yes"
+        return confirm
+      else
+        self.play
+      end
     end
   end
 end
